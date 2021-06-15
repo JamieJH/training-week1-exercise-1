f922a24af855a957f080acc8b00662ce22b8cb02
jamieminh <48439646+jamieminh@users.noreply.github.com>
2021-06-15 15:11:24 +0700

    fix footer, hide js variables

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-const toggleBtns = document.querySelector(".toggle-btns");
-const sidebar = document.querySelector(".sidebar");
+// const toggleBtns = document.querySelector(".toggle-btns");
+// const sidebar = document.querySelector(".sidebar");
 
-toggleBtns.addEventListener("click", function () {
-  sidebar.classList.toggle("close");
-  toggleBtns.classList.toggle("close")
-});
+// toggleBtns.addEventListener("click", function () {
+//   sidebar.classList.toggle("close");
+//   toggleBtns.classList.toggle("close")
+// });
+
+
+
+
+// self-called function
+(function () {  
+    var toggleBtns = document.querySelector(".toggle-btns");
+    var sidebar = document.querySelector(".sidebar");
+
+    toggleBtns.addEventListener("click", function () {
+        sidebar.classList.toggle("close");
+        toggleBtns.classList.toggle("close")
+    });
+})();
